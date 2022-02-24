758f9fd691e0bb0d95c8d8503fcbc5aa0c2414af
Jack Dockerty <jdockerty19@gmail.com>
2022-02-24 20:12:03 +0000

    add small helper script

--- helper.sh
+++ helper.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
+help_text="Options are:
 
+	create
+"
 
 case "$1" in
 
 	create)
 		echo "Creating template chapter structure"
-		
+
 		mkdir -p "$(pwd)/$2/images"
 		touch "$(pwd)/$2/exercises.md" "$(pwd)/$2/README.md"
 	;;
 
 	*)
-		echo 'Invalid arg'
+		echo "${help_text}"
+		
 esac
